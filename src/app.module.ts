ec48906011b0c416afef709a42a440fdaecc8438
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-03-01 20:22:21 +0700

    feat: setup swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AnggotaModule } from './anggota/anggota.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AnggotaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('OKK-Ristek API')
+    .setDescription('Tugas Open Recruitment OKK Ristek Fasilkom UI 2024')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // app.setGlobalPrefix('v1');
   await app.listen(3000);
 }
 bootstrap();
