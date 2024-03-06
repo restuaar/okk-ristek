5ad54af84c8bc919b76f0422850ea0fba155221f
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-03-06 18:34:22 +0700

    feat: setup deploy

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   // const { httpAdapter } = app.get(HttpAdapterHost);
   // app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
 
